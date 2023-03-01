eef7094229b3f21b029be263edde38b1392b6b9b
simo994q <simonkn1@hotmail.com>
2023-03-01 13:52:33 +0100

    done

--- assets/js.js
+++ assets/js.js
@@ -5,12 +5,20 @@ let confirmPass = document.getElementById('cPass')
 
 pass.addEventListener('keyup', () => {
     if (pass.value.length >= 6) {
-
+        pass.classList.add('valid')
+        pass.classList.remove('notValid')
+    } else {
+        pass.classList.add('notValid')
+        pass.classList.remove('valid')
     }
 })
 
 confirmPass.addEventListener('keyup', () => {
-    if (confirmPass.value = pass.value) {
-
+    if (confirmPass.value == pass.value) {
+        confirmPass.classList.add('valid')
+        confirmPass.classList.remove('notValid')
+    } else {
+        confirmPass.classList.add('notValid')
+        confirmPass.classList.remove('valid')
     }
 })
