a36522321ae598f205cd999d526a58bff3004250
simo994q <simonkn1@hotmail.com>
2023-03-01 13:35:51 +0100

    1st half

---
+++ assets/js.js
@@ -0,0 +1,16 @@
+
+
+let pass = document.getElementById('pass')
+let confirmPass = document.getElementById('cPass')
+
+pass.addEventListener('keyup', () => {
+    if (pass.value.length >= 6) {
+
+    }
+})
+
+confirmPass.addEventListener('keyup', () => {
+    if (confirmPass.value = pass.value) {
+
+    }
+})
